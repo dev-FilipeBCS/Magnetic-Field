5a24c420753cc7342e5a4675a22bca54c4d4c22a
Filipe Brega <filipebregaco@gmail.com>
2026-06-17 02:29:11 +0200

    Removed unnecessary sim_h variations; Added README.

--- src/fieldforge/simulation.py
+++ src/fieldforge/simulation.py
@@ -1,29 +1,6 @@
 import numpy as np
 
-#### VECTORIZED SIMULATION FUNCTIONS ####
-def sim_h_vectorized(sources, field_points):
-    n_obs = len(field_points)
-    h_total = np.zeros((n_obs, 3))
-    
-    for src in sources:
-        pos = src.pos
-        dl = src.dL
-        current = src.current
-
-        r_vec = field_points[:, np.newaxis, :] - pos[np.newaxis, :, :]  # (M, N, 3)
-        r = np.linalg.norm(r_vec, axis=2)
-        r = np.where(r < 1e-12, 1e-12, r)
-        r_hat = r_vec / r[:, :, np.newaxis]
-
-        dl_expanded = dl[np.newaxis, :, :]
-        cross_prod = np.cross(dl_expanded, r_hat, axis=2)
-
-        factor = current / (4 * np.pi * (r ** 2)[:, :, np.newaxis])
-        dh = factor * cross_prod
-        h_total += np.sum(dh, axis=1)
-    
-    return h_total
-
+#### VECTORIZED SIMULATION FUNCTIONs ####
 def sim_h(sources, field_points):
     n_obs = len(field_points)
     h_total = np.zeros((n_obs, 3))
@@ -49,37 +26,13 @@ def sim_h(sources, field_points):
     
     return h_total
 
-def sim_h_old(sources, field_points):
-    n_obs = len(field_points)
-    h = np.zeros((n_obs, 3))
-
-    for j in range(n_obs):
-        obs = field_points[j]
-        for src in sources:
-            pos = src.pos
-            dl = src.dL
-            current = src.current
-
-            dh = np.zeros_like(pos)
-            for i in range(len(pos)):
-                rv = obs - pos[i]
-                rs = np.linalg.norm(rv)
-                rs = max(rs, 1e-12)
-                ruv = rv / rs
-                dh[i] = current * np.cross(dl[i], ruv) / (4 * np.pi * rs**2)
-            h[j, :] += np.sum(dh, axis=0)
-    return h
-
-def sim_b(sources, field_points, vectorized=True):
+def sim_b(sources, field_points):
     """
     Compute magnetic field B = μ₀ * H
     Uses vectorized version by default.
     """
-    mu = np.pi * 4e-7 
-    if vectorized:
-        h = sim_h(sources, field_points)
-    else:
-        h = sim_h_old(sources, field_points)
+    mu = np.pi * 4e-7
+    h = sim_h(sources, field_points)
     b = h * mu
     return b
 
